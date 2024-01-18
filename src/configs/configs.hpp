79f0aa7d0d856f1f342b60f95b8cb9b3d902962a
M. Sami Gürpınar <sami.gurpinar@gmail.com>
2024-01-18 14:49:07 +0300

    refactor(configs): updated resolution

--- src/configs/configs.hpp
+++ src/configs/configs.hpp
@@ -9,5 +9,5 @@
 const std::string SHADER_DIR = "shaders/";
 const std::string ASSET_DIR = "assets/";
 
-constexpr unsigned int SCR_WIDTH = 800;
-constexpr unsigned int SCR_HEIGHT = 600;
+constexpr unsigned int SCR_WIDTH = 1600;
+constexpr unsigned int SCR_HEIGHT = 900;

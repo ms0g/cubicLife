66acd32adb8a580a0ce6257151254d359f156c5c
M. Sami Gürpınar <sami.gurpinar@gmail.com>
2024-02-01 05:20:04 +0300

    feat(cube): created cube class

---
+++ src/cube/cube.cpp
@@ -0,0 +1,13 @@
+#include "cube.h"
+
+Cube::Cube(Shader& shader) {
+    shader.activate();
+    shader.setInt(textures[0].name, 0);
+    shader.setInt(textures[1].name, 1);
+
+    mesh.setup(vertices, textures);
+}
+
+void Cube::draw() {
+    mesh.render();
+}
---
+++ src/cube/cube.h
@@ -0,0 +1,72 @@
+#pragma once
+
+#include <vector>
+#include "filesystem/filesystem.h"
+#include "../configs/configs.hpp"
+#include "../mesh/mesh.h"
+#include "../mesh/texture.h"
+#include "../shader/shader.h"
+
+class Cube {
+public:
+    explicit Cube(Shader& shader);
+
+    void draw();
+
+private:
+    Mesh mesh;
+#define TEX1 0.0
+#define TEX2 1.0
+    std::vector<float> vertices = {
+            -0.5f, -0.5f, -0.5f, 0.0f, 0.0f, TEX1,
+            0.5f, -0.5f, -0.5f, 1.0f, 0.0f, TEX1,
+            0.5f, 0.5f, -0.5f, 1.0f, 1.0f, TEX1,
+            0.5f, 0.5f, -0.5f, 1.0f, 1.0f, TEX1,
+            -0.5f, 0.5f, -0.5f, 0.0f, 1.0f, TEX1,
+            -0.5f, -0.5f, -0.5f, 0.0f, 0.0f, TEX1,
+
+            -0.5f, -0.5f, 0.5f, 0.0f, 0.0f, TEX1,
+            0.5f, -0.5f, 0.5f, 1.0f, 0.0f, TEX1,
+            0.5f, 0.5f, 0.5f, 1.0f, 1.0f, TEX1,
+            0.5f, 0.5f, 0.5f, 1.0f, 1.0f, TEX1,
+            -0.5f, 0.5f, 0.5f, 0.0f, 1.0f, TEX1,
+            -0.5f, -0.5f, 0.5f, 0.0f, 0.0f, TEX1,
+
+            -0.5f, 0.5f, 0.5f, 1.0f, 1.0f, TEX1,
+            -0.5f, 0.5f, -0.5f, 0.0f, 1.0f, TEX1,
+            -0.5f, -0.5f, -0.5f, 0.0f, 0.0f, TEX1,
+            -0.5f, -0.5f, -0.5f, 0.0f, 0.0f, TEX1,
+            -0.5f, -0.5f, 0.5f, 1.0f, 0.0f, TEX1,
+            -0.5f, 0.5f, 0.5f, 1.0f, 1.0f, TEX1,
+
+            0.5f, 0.5f, 0.5f, 1.0f, 1.0f, TEX1,
+            0.5f, 0.5f, -0.5f, 0.0f, 1.0f, TEX1,
+            0.5f, -0.5f, -0.5f, 0.0f, 0.0f, TEX1,
+            0.5f, -0.5f, -0.5f, 0.0f, 0.0f, TEX1,
+            0.5f, -0.5f, 0.5f, 1.0f, 0.0f, TEX1,
+            0.5f, 0.5f, 0.5f, 1.0f, 1.0f, TEX1,
+
+            -0.5f, -0.5f, -0.5f, 0.0f, 1.0f, TEX2,
+            0.5f, -0.5f, -0.5f, 1.0f, 1.0f, TEX2,
+            0.5f, -0.5f, 0.5f, 1.0f, 0.0f, TEX2,
+            0.5f, -0.5f, 0.5f, 1.0f, 0.0f, TEX2,
+            -0.5f, -0.5f, 0.5f, 0.0f, 0.0f, TEX2,
+            -0.5f, -0.5f, -0.5f, 0.0f, 1.0f, TEX2,
+
+            -0.5f, 0.5f, -0.5f, 0.0f, 1.0f, TEX2,
+            0.5f, 0.5f, -0.5f, 1.0f, 1.0f, TEX2,
+            0.5f, 0.5f, 0.5f, 1.0f, 0.0f, TEX2,
+            0.5f, 0.5f, 0.5f, 1.0f, 0.0f, TEX2,
+            -0.5f, 0.5f, 0.5f, 0.0f, 0.0f, TEX2,
+            -0.5f, 0.5f, -0.5f, 0.0f, 1.0f, TEX2,
+    };
+    std::vector<Texture> textures = {
+            {texture::load(fs::path(ASSET_DIR + "grass.png").c_str()),
+                    "texture1",
+                    "grass.png"},
+            {texture::load(fs::path(ASSET_DIR + "grass_top.png").c_str()),
+                    "texture2",
+                    "grass_top.png"}
+    };
+
+};

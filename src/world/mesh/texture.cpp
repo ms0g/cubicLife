480fc94f266334d28e5235e0a1d69958b0b11e8d
M. Sami Gürpınar <sami.gurpinar@gmail.com>
2024-02-27 19:01:58 +0300

    feat(texture): added loadcubemap

--- src/world/mesh/texture.cpp
+++ src/world/mesh/texture.cpp
@@ -38,3 +38,30 @@ unsigned int texture::load(const char* path) {
 
     return textureID;
 }
+
+unsigned int texture::loadCubemap(std::vector<std::string>& faces) {
+    unsigned int textureID;
+    glGenTextures(1, &textureID);
+    glBindTexture(GL_TEXTURE_CUBE_MAP, textureID);
+
+    int width, height, nrChannels;
+    for (unsigned int i = 0; i < faces.size(); i++) {
+        unsigned char* data = stbi_load(faces[i].c_str(), &width, &height, &nrChannels, 0);
+        if (data) {
+            glTexImage2D(GL_TEXTURE_CUBE_MAP_POSITIVE_X + i,
+                         0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data
+            );
+            stbi_image_free(data);
+        } else {
+            std::cout << "Cubemap tex failed to load at path: " << faces[i] << std::endl;
+            stbi_image_free(data);
+        }
+    }
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
+
+    return textureID;
+}
--- src/world/mesh/texture.h
+++ src/world/mesh/texture.h
@@ -1,5 +1,10 @@
 #pragma once
 
+#include <vector>
+#include <string>
+
 namespace texture {
-    unsigned load(const char* path);
+unsigned load(const char* path);
+
+unsigned int loadCubemap(std::vector <std::string>& faces);
 }

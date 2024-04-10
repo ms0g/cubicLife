0c17b3824d412dbd34942704952321df37ada537
M. Sami Gürpınar <sami.gurpinar@gmail.com>
2024-04-10 17:59:30 +0300

    refactor(world|skybox): minor readability improvement

--- src/world/skybox.cpp
+++ src/world/skybox.cpp
@@ -2,15 +2,14 @@
 #include "mesh/skyboxMesh.h"
 #include "../shader/shader.h"
 
-Skybox::Skybox() :
-        mShader(std::make_unique<Shader>(fs::path(SHADER_DIR + "skybox.vert.glsl"),
-                                         fs::path(SHADER_DIR + "skybox.frag.glsl"))) {
-
-    mShader->activate();
-    mShader->setInt(mTextures[0].name, 0);
-
+Skybox::Skybox() {
     mMesh = std::make_unique<SkyboxMesh>(mVertices, mTextures);
     mMesh->setup();
+
+    mShader = std::make_unique<Shader>(fs::path(SHADER_DIR + "skybox.vert.glsl"),
+                                       fs::path(SHADER_DIR + "skybox.frag.glsl"));
+    mShader->activate();
+    mShader->setInt(mTextures[0].name, 0);
 }
 
 Skybox::~Skybox() = default;

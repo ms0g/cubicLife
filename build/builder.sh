2789388caaf2e7a4f6add6e462fdcfa972c6ff3e
M. Sami Gürpınar <sami.gurpinar@gmail.com>
2024-04-19 23:39:54 +0300

    build(builder): added parallel compiling

--- build/builder.sh
+++ build/builder.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
-cmake -DCMAKE_BUILD_TYPE=Release .. && cmake --build .
+
+OS="$(uname -s)"
+if [ "$OS" = "Linux" ]; then
+  NPROCS="$(nproc --all)"
+elif [ "$OS" = "Darwin" ] || [ "$(echo "$OS" | grep -q BSD)" = "BSD" ]; then
+  NPROCS="$(sysctl -n hw.ncpu)"
+else
+  NPROCS="$(getconf _NPROCESSORS_ONLN)"
+fi
+
+
+cmake -DCMAKE_BUILD_TYPE=Release .. && cmake --build . --parallel "$NPROCS"

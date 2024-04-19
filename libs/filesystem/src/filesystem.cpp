e4f1280987c9c976db57218f912539e400762fb6
M. Sami Gürpınar <sami.gurpinar@gmail.com>
2024-04-19 21:45:59 +0300

    refactor(libs): minor

--- libs/filesystem/src/filesystem.cpp
+++ libs/filesystem/src/filesystem.cpp
@@ -3,6 +3,5 @@
 
 std::string fs::path(const std::string& p) {
     auto cwd = std::filesystem::current_path().parent_path();
-    //cwd.erase(cwd.find_last_of('/'));
     return cwd.append(p).string();
 }

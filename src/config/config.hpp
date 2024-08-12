3bf6cc8cc5a569133674a4784d13c1ec3487edb9
M. Sami Gürpınar <sami.gurpinar@gmail.com>
2024-08-13 01:22:32 +0300

    refactor(config): got rid of macro

--- src/config/config.hpp
+++ src/config/config.hpp
@@ -10,15 +10,15 @@ const std::string SHADER_DIR = "shaders";
 const std::string ASSET_DIR = "assets";
 
 // Screen Settings
-#define SCR_WIDTH 1600
-#define SCR_HEIGHT 900
-#define ASPECT (float) SCR_WIDTH / (float) SCR_HEIGHT
+constexpr float SCR_WIDTH{1600.0f};
+constexpr float SCR_HEIGHT{900.0f};
+constexpr float ASPECT{SCR_WIDTH / SCR_HEIGHT};
 
 // Camera Settings
-#define YAW            (-90.0f)
-#define PITCH          (-30.0f)
-#define SPEED           20.0f
-#define SENSITIVITY     0.1f
-#define ZOOM            45.0f
-#define ZNEAR           0.1f
-#define ZFAR            200.0f
+constexpr float YAW{-90.0f};
+constexpr float PITCH{-30.0f};
+constexpr float SPEED{20.0f};
+constexpr float SENSITIVITY{0.1f};
+constexpr float ZOOM{45.0f};
+constexpr float ZNEAR{0.1f};
+constexpr float ZFAR{200.0f};
